9d7e2b0d41c224915f419bab228e864c33cd9561
cw <christer.winge@gmail.com>
2021-12-01 15:04:48 +0100

    Day 1

--- Solutions/Day 01/src/day01/Main.java
+++ Solutions/Day 01/src/day01/Main.java
@@ -1,12 +1,8 @@
 package day01;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
